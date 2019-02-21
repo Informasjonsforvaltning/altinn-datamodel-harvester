566c09d73f5f849c181a7bf8aa6ab344e0764b5d
David Skogan <david.skogan@gmail.com>
2019-02-21 10:45:21 +0100

    Initial commit

---
+++ Controllers/SchemaController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using AltinnCore.Common.Factories.ModelFactory;
+using Manatee.Json;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Altinn.Service.Controllers
+{
+    /// <summary>
+    /// This controller contains utility operations to convert from xsd to jsonSchema
+    /// </summary>
+    public class SchemaController : Controller
+    {
+        /// <summary>
+        /// Returns the all production schemas in Altinn as JsonSchemas
+        /// </summary>
+        /// <returns>The schemas</returns>
+        [HttpGet]
+        [Route("api/v1/schemas")]
+        public async Task<IActionResult> Schemas()
+        {
+            AltinnServiceRepository repositoryClient = new AltinnServiceRepository();
+
+            Task<List<AltinnResource>> serviceRequestTask = AltinnServiceRepository.ReadAllSchemas();
+
+            await Task.WhenAll(serviceRequestTask);
+
+            if (serviceRequestTask.Result != null)
+            {
+                Manatee.Json.Serialization.JsonSerializer serializer = new Manatee.Json.Serialization.JsonSerializer();
+
+                JsonValue json = serializer.Serialize(serviceRequestTask.Result);
+
+                return Ok(json.GetIndentedString());
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("api/v1/convert")]        
+        public async Task<IActionResult> Convert()
+        {
+            XmlReaderSettings settings = new XmlReaderSettings
+            {
+                IgnoreWhitespace = true,
+            };
+
+            if (Request.ContentType.Contains("text/xml"))
+            {            
+                XmlReader doc = XmlReader.Create(Request.Body, settings);
+
+                // XSD to Json Schema
+                XsdToJsonSchema xsdToJsonSchemaConverter = new XsdToJsonSchema(doc, null);
+
+                Manatee.Json.Serialization.JsonSerializer serializer = new Manatee.Json.Serialization.JsonSerializer();
+
+                JsonValue json = serializer.Serialize(xsdToJsonSchemaConverter.AsJsonSchema());
+
+                return Ok(json.GetIndentedString());
+            }          
+            
+            return NotFound("Cannot read body. Needs to be XSD.");            
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace fdkalt
+{
+    public class Startup
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfiguration _config;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public Startup(IHostingEnvironment env, IConfiguration configuration, ILoggerFactory loggerFactory)
+        {
+            Configuration = configuration;
+            _env = env;
+            _loggerFactory = loggerFactory;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var logger = _loggerFactory.CreateLogger<Startup>();
+
+            if (_env.IsDevelopment())
+            {
+                // Development service configuration
+
+                logger.LogInformation("Development environment");
+            }
+            else
+            {
+                // Non-development service configuration
+
+                logger.LogInformation($"Environment: {_env.EnvironmentName}");
+            }
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseMvc();
+        }
+    }
+}
